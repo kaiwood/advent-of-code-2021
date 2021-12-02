0f604bcd76b612f4882c5439b48c593071e43939
Kai Wood <kwood@kwd.io>
2021-12-02 07:40:13 +0100

    Day 2

---
+++ day-2/index.ts
@@ -0,0 +1,54 @@
+import fs from "fs";
+
+type Command = [string, number];
+
+const instructions: Array<Command> = fs
+  .readFileSync(`${__dirname}/input.txt`, "utf-8")
+  .trim()
+  .split("\n")
+  .map((e) => e.split(" "))
+  .map((e) => [e[0], Number(e[1])]);
+
+/**
+ * Day 1
+ */
+const position = { x: 0, depth: 0, aim: 0 };
+
+for (let [dir, steps] of instructions) {
+  switch (dir) {
+    case "up":
+      position.depth -= steps;
+      break;
+    case "down":
+      position.depth += steps;
+      break;
+    case "forward":
+      position.x += steps;
+      break;
+  }
+}
+
+console.log(position.x * position.depth);
+
+/**
+ * Day 2
+ */
+position.x = 0;
+position.depth = 0;
+
+for (let [dir, steps] of instructions) {
+  switch (dir) {
+    case "up":
+      position.aim -= steps;
+      break;
+    case "down":
+      position.aim += steps;
+      break;
+    case "forward":
+      position.x += steps;
+      position.depth += position.aim * steps;
+      break;
+  }
+}
+
+console.log(position.x * position.depth);
